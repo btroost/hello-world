edf25e4acd61b271583d0a34930930f57db0c0c1
btroost <59251634+btroost@users.noreply.github.com>
2019-12-26 22:00:26 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb
+cgitb.enable()
+
+print ("Content-type: text/html\n\n")
+
+#print("Content-Type: text/plain;charset=utf-8")
+#print()
+
+print("Hello World!")
+print("<button type=""button"" onclick=""document.getElementById('demo').innerHTML = Date()"">Click me to display Date and Time.</button>")
+  #<button type="button" onclick="document.getElementById('demo').innerHTML = Date()">Click me to display Date and Time.</button>
+
+print("<p id=""demo"">xx</p>")
+  
